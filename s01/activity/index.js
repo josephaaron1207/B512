16f57357e320d0716c82a1b685797216f6c416e0
josephaarond.bernardo <josephaarond.bernardo@gmail.com>
2025-05-19 23:17:28 +0800

    Add solution for the s01 activity.

--- s01/activity/index.js
+++ s01/activity/index.js
@@ -1,1 +1,46 @@
-index.js
+let firstName = "Joseph Aaron";
+let lastName = 'Bernardo';
+let age = 26;
+let hobbies = "Table tennis, Video games, Anime"
+let myPlaylist = "Swicy - Unis, Cosmic - Red Velvet, Bahama - aespa"
+let myMovies = "Us, Shutter, As above so below"
+
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+console.log(hobbies);
+console.log(myPlaylist);
+console.log(myMovies);
+
+let game1 = {
+	name : "Rakion",
+	createdOn : 3/2005,
+	creator : "Softnyx",
+	publisher : "Softnyx",
+	totalSales : 824000,
+	isAvailable : true
+}
+
+console.log(game1);
+
+let game2 = {
+	name : "Dragon Nest SEA",
+	createdOn : 3/2010,
+	creator : "Eyedentity",
+	publisher : "Eyedentity",
+	totalSales : 9230000,
+	isAvailable : true
+}
+
+console.log(game2);
+
+let game3 = {
+	name : "Honkai Star Rail",
+	createdOn : 14/2023,
+	creator : "Hoyoverse",
+	publisher : "Hoyoverse",
+	totalSales : 1300000000,
+	isAvailable : true
+}
+
+console.log(game3);
